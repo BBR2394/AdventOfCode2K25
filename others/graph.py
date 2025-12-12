33c7bde4777a0df5c2f0d8cc64b0e53c6afb2f88
Baptiste BERTRAND-RAPELLO <baptiste.bertrand-rapello@dedalus.com>
2025-12-12 20:36:55 +0100

    bbr :progressing on graph one, and i started the prompt (it is easyer to dev with it

--- others/graph.py
+++ others/graph.py
@@ -6,7 +6,21 @@ import uuid
 class GraphNode:
 
     def DisplayInfo(self):
-        print("Id : ", self.id, "pseudo : ", self.easy_id," data : ", self.data_string)
+        print("Id : ", self.id, "pseudo : ", self.easy_id," data : ", self.data_string, "level : ", self.level)
+
+    def DisplayChild(self):
+        for i in self.list_child:
+            i.DisplayInfo()
+    
+    # def DisplayParent(self):
+    #     for i in self.list_parent:
+    #         i.DisplayInfo()
+
+    def GetShortId(self):
+        return str(self.id)[30:]
+
+    def CreateChildFromData(self, data="no data", pseudo="no pseudo"):
+        self.list_child.append(GraphNode(data, self.level+1))
 
     def __init__(self, data="", lvl=1, pseudo=""):
         self.id = uuid.uuid4()
@@ -35,6 +49,89 @@ class MaistreNode:
         self.name = name_given
         self.list_root_node = []
 
+def dynamicGenGraphe(masterNode=None):
+    tst=None #the cursor
+    
+    while True:
+        if tst != None:
+            print("[", tst.GetShortId(), "]")
+        entry = input("Que voulez-faire?\n--$>")
+
+        if entry == 'quit':
+            print("Au revoir")
+            return True
+        elif entry == 'add':
+            entry_bis = input("Que voulez-ajouter?\n--$>")
+            if tst:
+                tst.CreateChildFromData(data=entry_bis)
+            else : 
+                masterNode.GenOneMainChild(dataGvn=entry_bis)
+            #tst.addOneChild(dataStr=entry_bis, dataVal=42)
+        elif entry == 'display':
+            masterNode.DisplayRootNode()
+        elif entry == 'display_child':
+            if tst:
+                rtr = tst.DisplayChild()
+                print('retour : ', rtr)
+            else :
+                print("affichage des noeud principaux")
+                masterNode.DisplayRootNode()
+        elif entry == 'select_child':
+            if tst == None:
+                print("premiere initialisation")
+                masterNode.DisplayRootNode()
+            else :
+                tst.DisplayChild()
+                
+        elif entry == 'display_current':
+            if tst != None:
+                tst.DisplayInfo()
+            else :
+                print("cursor Not Init")
+        # elif entry == 'display_child':
+        #     rtr = tst.displayMyChild()
+        #     print('retour : ', rtr)
+        # elif entry == 'info':
+        #     rtr = tst.getInfo()
+        elif entry == 'moovein':
+            print("dans quel enfant se deplacer ? ")
+            if tst == None:
+                print("premiere initialisation")
+                masterNode.DisplayRootNode()
+            else :
+                tst.DisplayChild()
+
+            entry_bis = input("choississez\n--$>")
+            try:
+                entry_int = int(entry_bis)
+                if entry_int < 0:
+                    print("le nœud choisit ne peut etre negatif")
+                else:
+                    print("selection du noeud : ", entry_int)
+                    if tst == None:
+                        tst = masterNode.list_root_node[entry_int]
+                    else :
+                        if entry_int > len(tst.list_child):
+                            print("le nœud choisit ne peut dépasser le tableau")
+                        else:
+                            tst = tst.list_child[entry_int]
+            except:
+                print(" une erreur est survenu : avez vous choisit une bonne valeur ?")
+        elif entry == 'reset_cursor':
+            print("retour au nœud maitre")
+            tst = masterNode
+        # elif entry == "goto_parent":
+        #     if tst.hasParent():
+        #         tst = tst.parent
+        #     else : 
+        #         print(" on n'a pas de parent ")
+        # elif entry == "picture":
+        #     global extract
+        #     extract = []
+        #     genDataForPicture(main_node=masterNode, and_data=True)
+        #     fileName = "toto_dyn_graph"
+        #     genPicture(fileName=fileName)
+
 def main(av):
     print("run graph.py")
     mn = MaistreNode()
@@ -42,6 +139,10 @@ def main(av):
     mn.GenOneMainChild("au revoir", "Salutation départ")
     mn.GenOneMainChild("ntm", "Salutation insulte")
     mn.DisplayRootNode()
+
+    dynamicGenGraphe(mn)
+
+
 
 
 if __name__ == '__main__':
